2813fbd59d17c54c45acf9fc68303bb04554b588
andre <andrelima940@gmail.com>
2021-10-11 12:04:54 -0300

    remove date type menu

--- pages/cards/agenda.vue
+++ pages/cards/agenda.vue
@@ -1,131 +1,102 @@
 <template>
-<v-app style="background-color:rgba(0,0,0,1)">
-  <v-row class="fill-height">
-    <v-col>
-      <v-sheet 
-        class="cima"
-        height="64">
-        <v-toolbar
-          flat>
-          <v-btn
-            outlined
-            class="mr-4"
-            color="white"
-            @click="setToday">Hoje
-          </v-btn>
-          <v-btn
-            fab
-            text
-            small
-            color="white"
-            @click="prev"><v-icon>navigate_before</v-icon>
-          </v-btn>
-          <v-btn
-            fab
-            text
-            small
-            color="white"
-            @click="next"><v-icon>navigate_next</v-icon>
-          </v-btn>
-          <v-toolbar-title class="white--text" v-if="$refs.calendar">
-            {{ $refs.calendar.title }}
-          </v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-menu
-            bottom
-            right>
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                outlined
-                color="white"
-                v-bind="attrs"
-                v-on="on"
-              >
-                <span>{{ typeToLabel[type] }}</span>
-              </v-btn>
-            </template>
-            <v-list>
-              <v-list-item @click="type = 'day'">
-                <v-list-item-title>Dia</v-list-item-title>
-              </v-list-item>
-              <v-list-item @click="type = 'week'">
-                <v-list-item-title>Semana</v-list-item-title>
-              </v-list-item>
-              <v-list-item @click="type = 'month'">
-                <v-list-item-title>Mês</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-        </v-toolbar>
-      </v-sheet>
+  <v-app style="background-color:rgba(0,0,0,1)">
+    <v-row class="fill-height">
+      <v-col>
+        <v-sheet 
+          class="cima"
+          height="64">
+          <v-toolbar
+            flat>
+            <v-btn
+              outlined
+              class="mr-4"
+              color="white"
+              @click="setToday">Hoje
+            </v-btn>
+            <v-btn
+              fab
+              text
+              small
+              color="white"
+              @click="prev"><v-icon>navigate_before</v-icon>
+            </v-btn>
+            <v-btn
+              fab
+              text
+              small
+              color="white"
+              @click="next"><v-icon>navigate_next</v-icon>
+            </v-btn>
+            <v-toolbar-title class="white--text" v-if="$refs.calendar">
+              {{ $refs.calendar.title }}
+            </v-toolbar-title>
+          </v-toolbar>
+        </v-sheet>
 
 
 
-      <v-sheet
-        class="baixo"
-        height="600"
-        >
-        <v-calendar
-            class="rounded-lg calendar"
-            ref="calendar" 
-            v-model="focus"
-            color="rgba(255, 255, 255, 0.4)"  
-            :events="events"
-            :event-color="getEventColor"
-            :type="type"
-            @click:event="showEvent"
-            @click:mais="viewDay"
-            @click:date="viewDay"
-        >
-        </v-calendar>
-            
-        <v-menu
-          v-model="selectedOpen"
-          :close-on-content-click="false"
-          :activator="selectedElement"
-          offset-x>
-          <v-card
-            color="rgba(255, 255, 255, 0.9)"
-            min-width="350px"
-            flat
-            rounded="10"
+        <v-sheet
+          class="baixo"
+          height="600"
           >
-            <v-toolbar
-              color="rgba(0,0,0,0.5)">
-              <v-btn icon>
-                <v-icon>mdi-pencil</v-icon>
-              </v-btn>
-              <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
-              <v-spacer></v-spacer>
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-              <v-btn icon>
-                <v-icon>mdi-dots-vertical</v-icon>
-              </v-btn>
-            </v-toolbar>
-            <v-card-text >
-              <span class="black--text" v-html="selectedEvent.details"></span>
-            </v-card-text>
-            <v-card-actions>
-              <v-btn
-                text
-                color="secondary"
-                @click="selectedOpen = false">
-                Cancelar
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-menu>
-      </v-sheet>
-    </v-col>
-  </v-row>
-  <v-container class='my-16'></v-container>
-</v-app>
-
+          <v-calendar
+              class="rounded-lg calendar"
+              ref="calendar" 
+              v-model="focus"
+              color="rgba(255, 255, 255, 0.4)"  
+              :events="events"
+              :event-color="getEventColor"
+              :type="type"
+              @click:event="showEvent"
+              @click:mais="viewDay"
+              @click:date="viewDay"
+          >
+          </v-calendar>
+              
+          <v-menu
+            v-model="selectedOpen"
+            :close-on-content-click="false"
+            :activator="selectedElement"
+            offset-x>
+            <v-card
+              color="rgba(255, 255, 255, 0.9)"
+              min-width="350px"
+              flat
+              rounded="10"
+            >
+              <v-toolbar
+                color="rgba(0,0,0,0.5)">
+                <v-btn icon>
+                  <v-icon>mdi-pencil</v-icon>
+                </v-btn>
+                <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-btn icon>
+                  <v-icon>mdi-heart</v-icon>
+                </v-btn>
+                <v-btn icon>
+                  <v-icon>mdi-dots-vertical</v-icon>
+                </v-btn>
+              </v-toolbar>
+              <v-card-text >
+                <span class="black--text" v-html="selectedEvent.details"></span>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn
+                  text
+                  color="secondary"
+                  @click="selectedOpen = false">
+                  Cancelar
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-menu>
+        </v-sheet>
+      </v-col>
+    </v-row>
+    <v-container class='my-16'></v-container>
+  </v-app>
 </template>
-
-
 
 <script>
   export default {
@@ -214,25 +185,18 @@
 
 
 <style lang='sass'>
-
 .theme--light.v-sheet
    background: linear-gradient(to right, rgba(0,0,0,0), rgba(0,0,0,0))
-
 .calendar .v-outside
     background-color: transparent !important
-
 .theme--light.v-calendar-weekly
    background-color: transparent !important
-   
 .theme--light.v-application
    background-color: transparent !important
-
 .cima .v-sheet
     background-color: transparent !important
-
 .baixo .v-sheet
    background: linear-gradient(to right, rgba(0,0,0,0), rgba(0,0,0,0))
-
 .v-main 
   background: linear-gradient(to right, #663DEF, #E34BE2)
 .title
@@ -240,24 +204,17 @@
 .span
   display: inline-block
   line-height: 12px
-
 .v-calendar .v-btn__content 
     color: white
-
 .theme--light .v-calendar-weekly .v-calendar-weekly__head-weekday
     color: black !important
     border: none !important
-
 .theme--light .v-calendar-weekly
     border: none !important
-
 .theme--light .v-calendar-weekly .v-calendar-weekly__day
     border: rgba(255,255,255,0.5) 1px solid !important
-
 .v-calendar-daily
     background-color: transparent !important
-
 .v-menu__content
     border-radius: 30px !important
-
 </style>
