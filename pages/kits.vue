584dbfcf350f1c7c23127c6d0af6ad18dade18af
andre <andrelima940@gmail.com>
2021-10-14 21:55:09 -0300

    new page 'kits'

---
+++ pages/kits.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-app style="background-color:rgba(0, 0, 0, 0);">
+         
+    </v-app>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang='sass'>
+.v-main 
+  background: linear-gradient(to right, #663DEF, #E34BE2)
+.title
+  max-width: 500px
+.span
+  display: inline-block
+  line-height: 12px
+.filter-black
+  filter: invert(100%)
+.v-list-item
+    padding: 0
+.v-application .text-subtitle-1
+  width: 220px
+</style>
