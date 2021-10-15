53253f67ca5b76819a8a2f329915eb6e08388ade
andre <andrelima940@gmail.com>
2021-10-15 12:52:14 -0300

    consulta info update

--- pages/consulta.vue
+++ pages/consulta.vue
@@ -9,6 +9,19 @@
               class="mx-auto mt-13"
               max-width="800">
               <v-card-title class="font-weight-bold white--text pl-0"> Suas consultas </v-card-title>
+              <v-list-item target="_blank" href="">
+                  <v-list-item-content>
+                    <v-row class="ml-0 mr-0">
+                      <LabIcon class="pr-5 filter-black" height="20px"></LabIcon>
+                      <v-row class="ml-0 mr-0" justify="space-between" align="center">
+                          <div class="white--text text-subtitle-1 font-weight-bold">Dr. Gelol</div>
+                          <div class="white--text text-body-2"> Dermatologista </div>
+                          <div class="white--text text-overline align-center">08/10/2021</div>
+                      </v-row>  
+                    </v-row>                  
+                  </v-list-item-content>
+              </v-list-item>
+              <v-divider></v-divider>
               <v-list-item href="./consulta/ortopedista">
                   <v-list-item-content>
                     <v-row class="ml-0 mr-0">
@@ -17,19 +30,6 @@
                           <div class="white--text text-subtitle-1 font-weight-bold"> Dr. Luciano Rigoni Calleri </div>
                           <div class="white--text text-body-2"> Ortopedista </div>
                           <div class="white--text text-overline align-center">01/10/2021</div>
-                      </v-row>  
-                    </v-row>                  
-                  </v-list-item-content>
-              </v-list-item>
-              <v-divider></v-divider>
-              <v-list-item target="_blank" href="">
-                  <v-list-item-content>
-                    <v-row class="ml-0 mr-0">
-                      <LabIcon class="pr-5 filter-black" height="20px"></LabIcon>
-                      <v-row class="ml-0 mr-0" justify="space-between" align="center">
-                          <div class="white--text text-subtitle-1 font-weight-bold">Dr. João</div>
-                          <div class="white--text text-body-2"> Dermatologista </div>
-                          <div class="white--text text-overline align-center">08/10/2021</div>
                       </v-row>  
                     </v-row>                  
                   </v-list-item-content>
